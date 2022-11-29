989a7a3b9934fdbf97f7b85493e59d5cc7edcda0
Junyeong Yim <limejuny@gmail.com>
2022-11-30 00:25:37 +0900

    기본 media type 추가
    Detail
    ======
     - media-type: application/json

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo.config;
 
+import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,15 +8,31 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.responses.ApiResponse;
 
 @Configuration
 public class SwaggerConfig {
 
   @Bean
+  public OpenApiCustomiser customOpenApi() {
+    return openApi -> {
+      openApi.getPaths().values().forEach(pathItem -> pathItem.readOperations().forEach(operation -> {
+        operation.getResponses()
+            .addApiResponse("401", new ApiResponse()
+                .description("인증 정보가 없습니다."))
+            .addApiResponse("403", new ApiResponse()
+                .description("권한이 없습니다."))
+            .addApiResponse("500", new ApiResponse()
+                .description("서버 오류"));
+      }));
+    };
+  }
+
+  @Bean
   public OpenAPI springShopOpenAPI() {
     return new OpenAPI()
         .info(new Info().title("Spring Swagger API")
-            .description("테스트 프로젝트 입니다.")
+            .description("테스트 프로젝트입니다.")
             .version("v0.0.1")
             .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/")))
         .externalDocs(new ExternalDocumentation()
